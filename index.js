35a156bfeca45e555fc2c316cd15c208f7116176
njung <njung0723@gmail.com>
2020-03-01 17:27:28 +0900

    Enhanced security

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require("body-parser"); 
+const {User} = require("./models/User");
+const config =require('./config/key');
 
 const mongoose =require('mongoose');
-mongoose.connect('mongodb+srv://njung:test1234@youtube-clone-qciif.mongodb.net/test?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoose conected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
+//client 에서 application/x-www-form-urlencoded 형식으로 넘어오는 데이터를 분석해서 가지고 올 수 있도록 하는 것
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json 타입 데이터 
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => { 
+    const user = new User(req.body)
+    user.save((err, userInfo)=> {
+       if(err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: {
-        type: String, 
-        maxlength: 50
+        type: String,
+        maxlength: 50,
+        required: true
     },
     userId: {
         type: String,
-        unique: 1
+        unique: 1,
+        required: true
     },
     password: {
         type: String,
-        minlength: 5
+        minlength: 5,
+        required: true
     },
     classLevel: {
         type: String
