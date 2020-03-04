5ea8d2f948269c8682bcb7d4c3ae01a3c5ca9795
njung <njung0723@gmail.com>
2020-03-04 19:11:28 +0900

    add student's card in landing Page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Pagination, Card, Typography, Tag } from 'antd';
+import { Pagination, Card, Typography, Tag, Input } from 'antd';
+import { UserOutlined, EditOutlined, EllipsisOutlined, SettingOutlined  } from '@ant-design/icons';
 
 const { Title } = Typography;
 // const { Meta } = Card;
 
 function LandingPage() {
     const user = useSelector(state => state.user)
+    const stuArray = [
+        { name: '유재석', teacher: 'jenny', image: '', classLevel: 'beginner' },
+        { name: '강호동', teacher: 'rose', image: '', classLevel: 'beginner' },
+        { name: '김민석', teacher: 'lisa', image: '', classLevel: 'beginner' },        
+        { name: '유재석', teacher: 'jenny', image: '', classLevel: 'beginner' },
+        { name: '강호동', teacher: 'rose', image: '', classLevel: 'beginner' },
+        { name: '김민석', teacher: 'lisa', image: '', classLevel: 'beginner' },
+        { name: '유재석', teacher: 'jenny', image: '', classLevel: 'beginner' },
+        { name: '강호동', teacher: 'rose', image: '', classLevel: 'beginner' },
+        { name: '김민석', teacher: 'lisa', image: '', classLevel: 'beginner' },
+        { name: '유재석', teacher: 'jenny', image: '', classLevel: 'beginner' },
+        { name: '강호동', teacher: 'rose', image: '', classLevel: 'beginner' },
+        { name: '김민석', teacher: 'lisa', image: '', classLevel: 'beginner' },
+    ]
+    const Cards = stuArray.map((element, index) => {
+        console.log(element)
+        return (
+            <Card
+                style={{ width: 250, marginBottom: 10}}// height:300, margin: 10
+                title={`${element.name}`}
+                headStyle = {{fontWeight: "bold"}}
+                bodyStyle = {{fontWeight: "bold", padding: 10}}
+                hoverable                 
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                actions={[
+                    <EditOutlined key="edit" />,
+                    <SettingOutlined key="setting" />,                    
+                ]}
+                
+            >
+                {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
+                <p style={{ textAlign: 'center' }}>담당 선생님: {`${element.teacher}`} </p>
+                <p style={{ textAlign: 'center' }}>소속 반: {`${element.classLevel}`} </p>
+            </Card>
+        )
+    })
     const tag = (
         <div>
             <Tag>Tag 1</Tag>
@@ -22,6 +59,12 @@ function LandingPage() {
         </div>
     );
 
+    const stuSearch = (
+        <div className="example-input">
+            <Input size="large" placeholder="large size" prefix={<UserOutlined />} />
+        </div>
+    )
+
     if(user.userData && !user.userData.isAuth){
         return (
             <div>
@@ -32,21 +75,17 @@ function LandingPage() {
     }else{
 
         return (
-            <div>
-                
+            <div className="portfolio-div">
                 <Title level={3}>아이들 포트폴리오 list</Title>
                 {tag}
+                <br />
+
+                {stuSearch}
+                <br />
+                <br />
+
+                <div className="portfolio-card">{Cards}</div>
                 
-                <Card
-                    title="학생이름칸"
-                    hoverable
-                    style={{ width: 240 }}
-                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                >
-                    {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
-                    <p>담당 선생님: </p>
-                    <p></p>
-                </Card>
             </div>
         )
     }
